02e5d9f39a685fa9b3b4b0d507b880a4a68516c6
razanfnugroho <razanfnugroho.s@gmail.com>
2025-09-22 22:16:58 +0800

    Add files via upload

---
+++ UcapanVar.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class UcapanVar {
+    public static void main(String[] args) {
+        String salam="Halo ULM";
+        String visiUlm="ULM berdaya saing tinggi dan unggul di bidaang lahan basah";
+        String visiIlkom="Prodi ilkom ULM unggul dengan teknologi kekinian";
+               
+        
+        System.out.println(salam);
+        System.out.println(visiUlm);
+        System.out.println(visiIlkom);
+       
+        
+    }
+}
